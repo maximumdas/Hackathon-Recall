bfe25c752162453c4e3a8dd8e99250914ed06d5e
Nashir Wahyudi <nashirwahyudi@gmail.com>
2022-09-07 08:51:14 +0700

    shap data

--- nomenklatur/js/plot.js
+++ nomenklatur/js/plot.js
@@ -6,6 +6,14 @@ window.onload = () => {
 
   var url2 = "./../csv/boxplot_data_ipm.csv";
 
+  var urlSHAP = "./../csv/SHAP Values - All Data.csv";
+  var urlSHAPClust = [
+    "./../csv/SHAP Values - Cluster 1.csv",
+    "./../csv/SHAP Values - Cluster 2.csv",
+    "./../csv/SHAP Values - Cluster 3.csv",
+    "./../csv/SHAP Values - Cluster 4.csv",
+  ];
+
   //var url3 = 'XXXX';
   //var url4 = 'XXXX';
   //var url5 = 'XXXX';
@@ -15,22 +23,30 @@ window.onload = () => {
   var WIDTH_IN_PERCENT_OF_PARENT = 100,
     HEIGHT_IN_PERCENT_OF_PARENT = 97;
 
-  var gd3_div1 = d3.select("div[id='treemap']").style({
-    width: WIDTH_IN_PERCENT_OF_PARENT + "%",
-    "margin-left": (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + "%",
-    height: HEIGHT_IN_PERCENT_OF_PARENT + "vh",
-    "margin-top": (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + "vh",
-  });
+  var gd3_div1 = d3.select("div[id='treemap']");
+  // .style({
+  //   width: WIDTH_IN_PERCENT_OF_PARENT + "%",
+  //   "margin-left": (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + "%",
+  //   height: HEIGHT_IN_PERCENT_OF_PARENT + "vh",
+  //   "margin-top": (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + "vh",
+  // });
 
   var gd3_div2 = d3.select("div[id='boxplot']").style({
     width: WIDTH_IN_PERCENT_OF_PARENT + "%",
     "margin-left": (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + "%",
-    height: HEIGHT_IN_PERCENT_OF_PARENT + "vh",
     "margin-top": (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + "vh",
+  });
+
+  var gd3_div3 = d3.select("div[id='shap_all']").style({
+    // width: WIDTH_IN_PERCENT_OF_PARENT + "%",
+    // "margin-left": (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + "%",
+    // height: HEIGHT_IN_PERCENT_OF_PARENT + "vh",
+    // "margin-top": (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + "vh",
   });
 
   var my_Div1 = gd3_div1.node();
   var my_Div2 = gd3_div2.node();
+  var shapAll = gd3_div3.node();
 
   //URL1 and URL2
   function makeplot() {
@@ -39,6 +55,9 @@ window.onload = () => {
     });
     Plotly.d3.csv(url2, function (data) {
       createBoxPlot(data, "boxplot");
+    });
+    Plotly.d3.csv(urlSHAP, function (data) {
+      createShapPlot(data, "shap_all");
     });
   }
 
@@ -50,6 +69,52 @@ window.onload = () => {
     return result;
   }
 
+  // Shap value
+  function createShapPlot(data, divName, all = true) {
+    if (all) {
+      let xlabel = [],
+        yPosValues = [];
+      yNegValues = [];
+      baseNeg = [];
+      for (var i = 0; i < data.length; i++) {
+        xlabel.push(data[i].Variable);
+        if (data[i].Sign == "red") {
+          yPosValues.push(data[i].SHAP_abs);
+        } else {
+          yNegValues.push(data[i].SHAP_abs);
+          baseNeg.push(-1 * data[i].SHAP_abs);
+        }
+      }
+      var data = [
+        {
+          type: "bar",
+          x: xlabel,
+          y: yNegValues,
+          base: baseNeg,
+          hovertemplate: "%{base}",
+          marker: {
+            color: "red",
+          },
+          name: "Negative SHAP",
+        },
+        {
+          type: "bar",
+          x: xlabel,
+          y: yPosValues,
+          base: 0,
+          hovertemplate: "%{value}",
+          marker: {
+            color: "blue",
+          },
+          name: "Possitive SHAP",
+        },
+      ];
+
+      Plotly.newPlot(divName, data);
+    }
+  }
+
+  // Boxplot
   function createBoxPlot(data, divName) {
     let clusts = [];
     for (var i = 0; i < CLUSTERS; i++) {
@@ -62,6 +127,7 @@ window.onload = () => {
     Plotly.newPlot(divName, clusts);
   }
 
+  // Treemap
   function processData(data, divName) {
     let labels = [],
       parents = [],
@@ -107,5 +173,6 @@ window.onload = () => {
   window.onresize = function () {
     Plotly.Plots.resize(my_Div1);
     Plotly.Plots.resize(my_Div2);
+    Plotly.Plots.resize(shapAll);
   };
 };
